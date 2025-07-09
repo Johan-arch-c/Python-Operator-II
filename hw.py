245100fc52e9b4b172f2d34455acf457bb10fe52
Johan-arch-c <johannithya@gmail.com>
2025-07-09 18:27:39 +1000

    hw

---
+++ hw.py
@@ -0,0 +1,32 @@
+a=1
+b=2
+c=2
+
+print(a !=b)
+print(b !=c)
+
+
+a="python"
+b="coding"
+
+if a != b:
+    print(a, 'and',b,'are different.')
+
+
+
+a=3
+b=4
+
+
+if (a==1) !=(b==5):
+    print('hello')
+
+
+
+
+    
+a=int(input("enter a number"))
+
+
+if a%2 !=0:
+    print(a, "is not even number.")
